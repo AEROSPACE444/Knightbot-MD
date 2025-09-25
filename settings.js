e9a94dd7f294fcf7954a82853aceea8741d59cf6
AEROSPACE444 <tanatswakole@gmail.com>
2025-09-25 15:07:32 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "STENA-XMD",
   botOwner: 'FRADRICK MUSAKATIZA, // Your name
-  ownerNumber: '263780492124', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '263773169536', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
